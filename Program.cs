a4f66c042e3c3369263f3c07cdfd272b4745186b
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-11 05:18:51 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,31 @@
+using FinalProject;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<FinalProjectContext>(
+    options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
+
+//Database connection
+//builder.Services.AddDbContext<FinalProjectContext>(options => 
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//
+//});
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
