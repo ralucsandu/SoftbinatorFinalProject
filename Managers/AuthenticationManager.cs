1e4307f24d1d05c9358ab79fd47b57f7270b71c0
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 16:56:26 +0300

    Update AuthenticationManager.cs

--- Managers/AuthenticationManager.cs
+++ Managers/AuthenticationManager.cs
@@ -1,6 +1,50 @@
-﻿namespace FinalProject.Managers
+using FinalProject.Managers;
+using FinalProject.Entities;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace FinalProject.Managers
 {
-    public class AuthenticationManager
+    public class AuthenticationManager : IAuthenticationManager
     {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+        private readonly ITokenManager tokenManager;
+        public AuthenticationManager(UserManager<User> userManager, SignInManager<User> signInManager,
+            ITokenManager tokenManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.tokenManager = tokenManager;
+        }
+        public async Task Signup(SignupUserModel signupUserModel)
+        {
+            var user = new User
+            {
+                Email = signupUserModel.Email,
+                UserName = signupUserModel.Email
+            };
+            var result = await userManager.CreateAsync(user, signupUserModel.Password);
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(user, signupUserModel.RoleId);
+            }
+        }
+        public async Task<TokenModel> Login(LoginUserModel loginUserModel)
+        {
+            var user = await userManager.FindByEmailAsync(loginUserModel.Email);
+            if (user != null)
+            {
+                var result = await signInManager.CheckPasswordSignInAsync(user, loginUserModel.Password, false);
+                if (result.Succeeded)
+                {
+                   
+                    var token = await tokenManager.CreateToken(user);
+                    return new TokenModel { Token = token };
+                }
+            }
+            return null;
+        }
     }
 }
