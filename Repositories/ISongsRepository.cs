f0cb256f044790ab54302aa56f1a5d19cfc9594c
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 14:56:18 +0300

    Update ISongsRepository.cs

--- Repositories/ISongsRepository.cs
+++ Repositories/ISongsRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace FinalProject.Repositories
+using FinalProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Repositories
 {
     public interface ISongsRepository
     {
+        IQueryable<Song> GetSongsIQueryable();
+        void CreateSong(Song song);
+        void UpdateSong(Song song);
+        void DeleteSong(Song song);
     }
 }
