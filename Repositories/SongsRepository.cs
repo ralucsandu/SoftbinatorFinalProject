c11316f6db869cb8ee68b106b58bda31567270af
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 14:56:00 +0300

    Update SongsRepository.cs

--- Repositories/SongsRepository.cs
+++ Repositories/SongsRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace FinalProject.Repositories
+using FinalProject.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Repositories
 {
-    public class SongsRepository
+    public class SongsRepository : ISongsRepository
     {
+        private FinalProjectContext db;
+
+        public SongsRepository(FinalProjectContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<Song> GetSongsIQueryable()
+        {
+            var songs = db.Songs;
+            return songs;
+        }
+
+        public void CreateSong(Song song)
+        {
+            db.Songs.Add(song);
+            db.SaveChanges();
+        }
+
+        public void UpdateSong(Song song)
+        {
+            db.Songs.Update(song);
+            db.SaveChanges();
+        }
+
+        public void DeleteSong(Song song)
+        {
+            db.Songs.Remove(song);
+            db.SaveChanges();
+        }
+
     }
 }
