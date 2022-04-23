3760f805348b1ce1da9b943bae4f7d5160789419
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 14:59:18 +0300

    Update IGiftsManager.cs

--- Managers/IGiftsManager.cs
+++ Managers/IGiftsManager.cs
@@ -1,6 +1,19 @@
-﻿namespace FinalProject.Managers
+using FinalProject.Entities;
+using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Managers
 {
     public interface IGiftsManager
     {
+        List<GiftModel> GetGifts();
+        List<int> GetGiftIdsList();
+        Gift GetGiftById(int id);
+        void CreateGift(GiftModel concertModel);
+        void DeleteGift(int GiftId);
+
     }
 }
