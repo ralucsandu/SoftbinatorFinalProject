0b485b2db24720a03022581c19f5ada50223917e
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 14:55:31 +0300

    Update IGiftsRepository.cs

--- Repositories/IGiftsRepository.cs
+++ Repositories/IGiftsRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace FinalProject.Repositories
+using FinalProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Repositories
 {
     public interface IGiftsRepository
     {
+        IQueryable<Gift> GetGiftsIQueryable();
+        void CreateGift(Gift gift);
+        void DeleteGift(Gift gift);
     }
 }
