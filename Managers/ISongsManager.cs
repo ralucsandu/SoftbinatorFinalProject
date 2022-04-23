b9a3aa4d73abb67afa39662bbfb659965d6c1703
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 15:00:09 +0300

    Update ISongsManager.cs

--- Managers/ISongsManager.cs
+++ Managers/ISongsManager.cs
@@ -1,6 +1,20 @@
-﻿namespace FinalProject.Managers
+using FinalProject.Entities;
+using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Managers
 {
     public interface ISongsManager
     {
+        List<SongModel> GetSongs();
+        List<int> GetSongIdsList();
+        Song GetSongById(int id);
+        void CreateSong(SongModel obiectModel);
+        void UpdateSong(SongModel obiectModel);
+        void DeleteSong(int SongId);
+
     }
 }
