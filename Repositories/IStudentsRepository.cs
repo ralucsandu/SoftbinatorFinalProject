e24f849c25edf691a688c0ecf563df29336aed4e
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 15:33:13 +0300

    Update IStudentsRepository.cs

--- Repositories/IStudentsRepository.cs
+++ Repositories/IStudentsRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace FinalProject.Repositories
+using FinalProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Repositories
 {
     public interface IStudentsRepository
     {
+        IQueryable<Student> GetStudentsIQueryable();
+        void CreateStudent(Student student);
+        void UpdateStudent(Student student);
+        void DeleteStudent(Student student);
     }
 }
