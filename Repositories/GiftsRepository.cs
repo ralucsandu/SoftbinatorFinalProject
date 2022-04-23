6c6245ac0931e7cbb6ab1bddafcdf6022ab906d2
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 14:55:14 +0300

    Update GiftsRepository.cs

--- Repositories/GiftsRepository.cs
+++ Repositories/GiftsRepository.cs
@@ -1,6 +1,38 @@
-﻿namespace FinalProject.Repositories
+using FinalProject.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Repositories
 {
-    public class GiftsRepository
+    public class GiftsRepository : IGiftsRepository
     {
+        private FinalProjectContext db;
+
+        public GiftsRepository(FinalProjectContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<Gift> GetGiftsIQueryable()
+        {
+            var gifts = db.Gifts;
+            return gifts;
+        }
+
+        public void CreateGift(Gift gift)
+        {
+            db.Gifts.Add(gift);
+            db.SaveChanges();
+        }
+
+        public void DeleteGift(Gift gift)
+        {
+            db.Gifts.Remove(gift);
+            db.SaveChanges();
+        }
+
     }
 }
