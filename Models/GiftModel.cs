207287dc07eb072b7f71c03e144a12738b0eccc2
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 14:58:17 +0300

    Update GiftModel.cs

--- Models/GiftModel.cs
+++ Models/GiftModel.cs
@@ -1,6 +1,13 @@
-﻿namespace FinalProject.Models
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
 {
     public class GiftModel
     {
+        public int Id { get; set; }
+
     }
 }
