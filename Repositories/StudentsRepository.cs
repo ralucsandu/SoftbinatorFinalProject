d445bcc10231acfcfdf9869f5db534af1ba940d9
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 15:32:27 +0300

    Update StudentsRepository.cs

--- Repositories/StudentsRepository.cs
+++ Repositories/StudentsRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace FinalProject.Repositories
+using FinalProject.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Repositories
 {
-    public class StudentsRepository
+    public class StudentsRepository : IStudentsRepository
     {
+        private FinalProjectContext db;
+
+        public StudentsRepository(FinalProjectContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<Student> GetStudentsIQueryable()
+        {
+            var students = db.Students;
+            return students;
+        }
+
+        public void CreateStudent(Student student)
+        {
+            db.Students.Add(student);
+            db.SaveChanges();
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            db.Students.Update(student);
+            db.SaveChanges();
+        }
+
+        public void DeleteStudent(Student student)
+        {
+            db.Students.Remove(student);
+            db.SaveChanges();
+        }
+
     }
 }
