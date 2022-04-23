c53fe08516bee1c3412e6712647311dd687935c7
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 15:35:16 +0300

    Update StudentsManager.cs

--- Managers/StudentsManager.cs
+++ Managers/StudentsManager.cs
@@ -1,6 +1,72 @@
-﻿namespace FinalProject.Managers
+using FinalProject.Entities;
+using FinalProject.Models;
+using FinalProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FinalProject.Managers
 {
-    public class StudentsManager
+    public class StudentsManager : IStudentsManager
     {
+        private readonly IStudentsRepository studentsRepository;
+        public StudentsManager(IStudentsRepository studentsRepository)
+        {
+            this.studentsRepository = studentsRepository;
+        }
+        public List<StudentModel> GetStudents()
+        {
+            var students = studentsRepository.GetStudentsIQueryable()
+                .Select(x => new StudentModel
+                {
+                    Id = x.Id,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName
+                })
+                .ToList();
+            return students;
+        }
+        public List<int> GetStudentIdsList()
+        {
+            var students = studentsRepository.GetStudentsIQueryable();
+            var idList = students.Select(x => x.Id)
+                .ToList();
+            return idList;
+        }
+
+        public Student GetStudentById(int id)
+        {
+            var student = studentsRepository.GetStudentsIQueryable()
+                .FirstOrDefault(x => x.Id == id);
+            return student;
+        }
+
+        public void CreateStudent(StudentModel studentModel)
+        {
+            var newStudent = new Student
+            {
+                FirstName = studentModel.FirstName,
+                LastName = studentModel.LastName
+            };
+
+            studentsRepository.CreateStudent(newStudent);
+        }
+
+        public void UpdateStudent(StudentModel studentModel)
+        {
+            var student = GetStudentById(studentModel.Id);
+            student.FirstName = studentModel.FirstName;
+            student.LastName = studentModel.LastName;
+            studentsRepository.UpdateStudent(student);
+        }
+
+        public void DeleteStudent(int Id)
+        {
+            var student = GetStudentById(Id);
+            studentsRepository.DeleteStudent(student);
+        }
+
     }
 }
