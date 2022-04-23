5bb144c8f51b81343774c2fbde982aa35e93d085
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-23 16:52:09 +0300

    Add LoginUserModel, SignupUserModel

---
+++ Models/LoginUserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FinalProject.Models
+{
+    public class LoginUserModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/SignupUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalProject.Models
+{
+    public class SignupUserModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RoleId { get; set; }
+    }
+}
