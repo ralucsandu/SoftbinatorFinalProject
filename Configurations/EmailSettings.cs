88c89a73caaeed137b521be883225780f19d1446
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-26 15:57:47 +0300

    Add Configurations

---
+++ Configurations/EmailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace FinalProject.Configurations
+{
+    public class EmailSettings
+    {
+        public string EmailId { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public bool UseSSL { get; set; }
+    }
+}
