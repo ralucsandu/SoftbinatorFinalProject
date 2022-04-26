713cb4dbc1184a638aa57229d6e3af8ef51323ef
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-26 15:05:26 +0300

    Add EmailController.cs

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,24 @@
+﻿using FinalProject.Models;
+using FinalProject.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {
+        IEmailService _emailService = null;
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+        [HttpPost]
+        public bool SendEmail(EmailData emailData)
+        {
+            return _emailService.SendEmail(emailData);
+        }
+    }
+}
