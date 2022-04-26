6136342eff8685d689f399c339b002ee85cb8a22
Raluca-Ioana Sandu <79568914+ralucsandu@users.noreply.github.com>
2022-04-26 15:56:03 +0300

    Add ViewModels, View for Upload

---
+++ ViewModels/ConcertCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FinalProject.ViewModels
+{
+    public class ConcertCreateViewModel
+    {
+
+        public string ConcertName { get; set; }
+        public string Date { get; set; }
+        public string Location { get; set; }
+        public IFormFile Photo { get; set; }
+
+
+    }
+}
